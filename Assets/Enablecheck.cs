00bed0d5e4140259730fca9c369bb306e5b34fe0
Sahithi <sahithibandi74@gmail.com>
2023-11-11 08:03:02 -0700

    Added Enablecheck

---
+++ Assets/Enablecheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enablecheck : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("GoalKey"))
+        {
+            if (obj.GetComponent<OVRGrabbable>().isGrabbed)
+            {
+                obj.SetActive(true);
+            }
+        }
+        
+    }
+}
